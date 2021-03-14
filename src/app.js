3be633be88d2de717c22ac0aac311c94219783e5
KarandeepSinghCoder <karandchandi@gmail.com>
2021-03-14 13:05:25 +0530

    final temp

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.engine("ejs", require("ejs").__express);
 
+// Import routers
+const dashboardRoutes = require("./dashboard/dashboardControler");
+const userRouters = require("./user/userControler");
+//Use imported routes
+app.use("/dashboard", dashboardRoutes);
+app.use("/user", userRouters);
 app.get("/", function(req, res) {
   res.render("landing.ejs");
 });
