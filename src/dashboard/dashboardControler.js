3dee8c6bbaa47e67ea39dc362dfc57508b8fdf9e
unknown <karandchandi@gmail.com>
2021-03-22 23:20:19 -0700

    setting up

--- src/dashboard/dashboardControler.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Dashboard controler routes
-router.get("/", function(req, res) {
-  res.render("dashboard/dashboard.ejs");
-});
-
-module.exports = router;
