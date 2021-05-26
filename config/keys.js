923fcaa1fd13b4d7e97a55222b8f98be88523380
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2021-05-26 20:49:45 +0530

    remove

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// dbPassword =
-// 	'mongodb+srv://kdsinghcoder:kdsinghcoder@cluster.fewrr.mongodb.net/myDatabase?retryWrites=true&w=majority';
-
-
-dbPassword = 'mongodb://localhost/dbPassword';
-module.exports = {
-    mongoURI: dbPassword
-};
